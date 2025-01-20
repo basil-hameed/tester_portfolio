8c4d46abf178d0e1000b9eca28789d7aecfb49c9
Basil Hameed <150420259+basil-hameed@users.noreply.github.com>
2025-01-20 11:42:27 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("Portfolio Loaded!");
-});
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("Portfolio Loaded!");
-});
